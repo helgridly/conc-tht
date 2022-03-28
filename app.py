236c711c97544b2d71b74f897cb176b502566b6b
Hussein Elgridly <h.elgridly@gmail.com>
2022-03-28 14:51:51 +0000

    add swagger

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import flask
 from flask import Flask
+from flask_restx import Api, Resource, fields
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
 
+routes = flask.Blueprint('tube-service', __name__)
+api = Api(routes, version='0.1', title='Tubes Service',
+          description='tubes service')
 
-class User(db.Model):
+ns = api.namespace('/', description='tube handling')
+app.register_blueprint(routes)
+
+class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
 
@@ -15,9 +23,10 @@ class Tube(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     barcode = db.Column(db.String, unique=True, nullable=False)
     status = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey(Patient.id), nullable=False)
 
 
-@app.route("/")
-def hello_world():
-    return {"hello": "world"}
+@ns.route("/tubes")
+class Tubes(Resource):
+    def get(self):
+        return {"hello": "world"}
